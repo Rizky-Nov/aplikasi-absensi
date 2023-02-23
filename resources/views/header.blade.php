fb84379a56b07bce23562c2ffb8f551fb01f33a6
Rizky-Nov <rizky04noviwidiyanto04@gmail.com>
2023-02-23 14:21:38 +0700

    Ini lagi apa

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,25 +3,24 @@
         <div class="judul col-12 header-m p-0 m-0">
             <div class="col-6 d-flex">
                 <div class="kembali col d-flex justify-content-center align-items-start">
-                    <a href="/home" class="header-m text-neutral-10">Home</a>
+                    <a href="/home" class="header-s text-neutral-10">Home</a>
                 </div>
                 <div class="nama col-10">
                     <p class="text-neutral-10 header-l">@yield('judul')</p>
                 </div>
             </div>
 
-            <div class="PL col-6">
-                <div class="profile col d-flex justify-content-end">
-                    <a href="/profile" class="kotak-profile d-flex justify-content-center align-items-center">
-                        <img src="{{ asset('gambar/profile-contoh.jpg') }}" alt="">
-                    </a>
-                    <span href="/profile" class="header-s text-neutral-10 d-flex align-items-center" style="margin-left: 8px">Ini Adalah Nama Pengguna</span>
-                </div>
-
-                <div class="logout col-2 d-flex justify-content-end">
-                    <a href="/logout">
-                        <img src="{{ asset('icon/logout.png') }}" style="height: 30px; width: 30px; filter: invert(100%)" alt="">
-                    </a>
+            <div class="col d-flex align-items-end flex-column">
+                <div class="PL col-7 me-4 d-flex p-0 m-0 h-100">
+                    <div class="w-100" style="position: relative;">
+                        <a href="/home">
+                            <img src="{{ asset('gambar/profile-contoh.jpg') }}" class="kotak-profile me-2" alt="">
+                        </a>
+            
+                        <a class="apaini text-m-medium text-neutral-10 w-100">
+                            <span class="header-s text-neutral-10">Ini Adalah Nama Pengguna</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,18 +34,25 @@
                 <p class="text-neutral-10 header-l">@yield('judul')</p>
             </div>
 
-            <div class="PL col-6">
-                <div class="profile col d-flex justify-content-end">
-                    <a href="/profile" class="kotak-profile d-flex justify-content-center align-items-center">
-                        <img src="{{ asset('gambar/profile-contoh.jpg') }}" class="w-100 h-100" alt="">
-                    </a>
-                    <span href="/profile" class="header-s text-neutral-10 d-flex align-items-center" style="margin-left: 8px">Ini Adalah Nama Pengguna</span>
-                </div>
+            <div class="col d-flex align-items-end flex-column">
+                <div class="PL col-7 me-4 d-flex p-0 m-0 h-100">
+                    <div class="w-100" style="position: relative;">
+                        <a href="/profile">
+                            <img src="{{ asset('gambar/profile-contoh.jpg') }}" class="kotak-profile me-2" alt="">
+                        </a>
+            
+                        <span class="header-s text-neutral-10">Ini Adalah Nama Pengguna</span>
 
-                <div class="logout col-2 d-flex justify-content-end">
-                    <a href="/logout">
-                        <img src="{{ asset('icon/logout.png') }}" style="height: 30px; width: 30px; filter: invert(100%)" alt="">
-                    </a>
+                        <a class="apaini text-m-medium text-neutral-10 cursor-pointer" data-bs-toggle="collapse" data-bs-target="#dropdown-menu-logout">
+                            <i class="fa-solid fa-ellipsis-vertical ms-3" style="font-size: 20px; width: 30px" aria-hidden="true"></i>
+                        </a>
+            
+                        <div class="logout w-100 collapse  {{ Request::is('logout') ? 'show' : '' }}" style="margin-right: 36px; position: absolute" id="dropdown-menu-logout">
+                            <a href="/logout" 
+                            class="text-m-regular w-100 d-flex justify-content-end text-neutral-10" 
+                            style="padding-right: 48px">Log-Out</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,89 +1,89 @@
 <form wire:submit.prevent='setStatus' class="col d-flex">
-    @csrf
-    <div class="col-3 d-flex flex-column align-items-center">
-      <div class="foto-profile">
-        <img src="{{ asset('gambar/profile-contoh.jpg') }}" class="w-100 h-100" alt="">
+  @csrf
+  <div class="col-3 d-flex flex-column align-items-center">
+    <div class="foto-profile">
+      <img src="{{ asset('gambar/profile-contoh.jpg') }}" class="w-100 h-100" alt="">
+    </div>
+    <div class="akunnya w-100">
+      <div class="form-group w-100">
+        <label for="email">Email</label>
+        <input disabled type="text" id="email" value="{{ auth()->user()->email }}" 
+        class="form-control w-100" placeholder="Email">
       </div>
-      <div class="akunnya w-100">
-        <div class="form-group w-100">
-          <label for="email">Email</label>
-          <input disabled type="text" id="email" value="{{ auth()->user()->email }}" 
-          class="form-control w-100" placeholder="Email">
-        </div>
-    
-        <div class="form-group w-100">
-        <label for="pw">Password</label>
-        <input disabled type="password" value="{{ $password }}"
-        id="pw" class="form-control w-100" placeholder="Password">
-        </div>
+  
+      <div class="form-group w-100">
+      <label for="pw">Password</label>
+      <input disabled type="password" value="{{ $password }}"
+      id="pw" class="form-control w-100" placeholder="Password">
       </div>
     </div>
-  
-    <div class="col-8 d-flex flex-column align-items-center" style="gap: 36px">
-      <div class="col-8">
-        {{-- <span class="w-100 d-flex justify-content-center header-s text-neutral-90">{{ auth()->user()->kelas->nama_tempat }}</span> --}}
-  
-        <div class="separator w-100 mt-2"></div>
-      </div>
+  </div>
 
-      <div class="col-10">
-        <div class="data-diri-profile col-12">
-    
-          <div class="input-profile">
-            <div class="form-group" style="width: 411px">
-              <label for="nama-profile">Nama Lengkap</label>
-              <input type="text" {{ $status ? '' : "disabled" }} wire:model='namalengkap' 
-              id="nama-profile" class="form-control w-100" placeholder="nama lengkap anda">
-            </div>
-  
-            <div class="form-group" style="width: 305px">
-              <label for="No-profile">No Telephone</label>
-              <input type="text" {{ $status ? '' : "disabled" }} wire:model='notelp'
-              id="No-profile" class="form-control w-100" placeholder="+62 812 8429 2341">
-            </div>
-          </div>
-  
-          <div class="input-profile">
-            <div class="form-group" style="width: 250.5px">
-              <label for="TL-profile">Tempat Lahir</label>
-              <input type="text" {{ $status ? '' : "disabled" }} wire:model='tempatlahir'
-              id="TL-profile" class="form-control w-100" placeholder="tempat lahir anda">
-            </div>
-  
-            <div class="form-group" style="width: 250.5px">
-              <label for="TGL-profile">Tanggal Lahir</label>
-              <input type="date" {{ $status ? '' : "disabled" }} wire:model='tgllahir'
-              id="TGL-profile" class="form-control w-100" placeholder="Tanggal Lahir">
-            </div>
-          </div>
-  
-          <div class="input-profile">
-            <div class="form-group" style="width: 304.5px">
-              <label for="Agama-profile">Agama</label>
-              <input type="text" {{ $status ? '' : "disabled" }} wire:model='agama'
-              id="Agama-profile" class="form-control w-100" placeholder="agama">
-            </div>
-  
-            <div class="form-group" style="width: 304.5px">
-              <label for="JK-profile">Jenis Kelamin</label>
-              <input type="text" {{ $status ? '' : "disabled" }} wire:model='jk'
-              id="JK-profile" class="form-control w-100" placeholder="jenis kelamin">
-            </div>
-          </div>
-  
-          <div class="form-group w-100">
-            <label for="alamat">Alamat</label>
-            <textarea {{ $status ? '' : "disabled" }} wire:model='alamat'
-            id="alamat" rows="3" class="w-100 border p-2 rounded-2" placeholder="alamat anda"></textarea>
-          </div>
-  
-        </div>
-      </div>
-      
+  <div class="col-8 d-flex flex-column align-items-center" style="gap: 36px">
+    <div class="col-8">
+      {{-- <span class="w-100 d-flex justify-content-center header-s text-neutral-90">{{ auth()->user()->kelas->nama_tempat }}</span> --}}
+
+      <div class="separator w-100 mt-2"></div>
     </div>
 
-    <div class="col w-100 d-flex justify-content-center" style="height: 36px;">
-        <button class="btn text-neutral-10 text-m-regular w-100"
-        style="background: #4ca1af;" type="submit">Profile Edit</button>
+    <div class="col-10">
+      <div class="data-diri-profile col-12">
+  
+        <div class="input-profile">
+          <div class="form-group" style="width: 411px">
+            <label for="nama-profile">Nama Lengkap</label>
+            <input type="text" {{ $status ? '' : "disabled" }} wire:model='namalengkap' 
+            id="nama-profile" class="form-control w-100" placeholder="nama lengkap anda">
+          </div>
+
+          <div class="form-group" style="width: 305px">
+            <label for="No-profile">No Telephone</label>
+            <input type="text" {{ $status ? '' : "disabled" }} wire:model='notelp'
+            id="No-profile" class="form-control w-100" placeholder="+62 812 8429 2341">
+          </div>
+        </div>
+
+        <div class="input-profile">
+          <div class="form-group" style="width: 250.5px">
+            <label for="TL-profile">Tempat Lahir</label>
+            <input type="text" {{ $status ? '' : "disabled" }} wire:model='tempatlahir'
+            id="TL-profile" class="form-control w-100" placeholder="tempat lahir anda">
+          </div>
+
+          <div class="form-group" style="width: 250.5px">
+            <label for="TGL-profile">Tanggal Lahir</label>
+            <input type="date" {{ $status ? '' : "disabled" }} wire:model='tgllahir'
+            id="TGL-profile" class="form-control w-100" placeholder="Tanggal Lahir">
+          </div>
+        </div>
+
+        <div class="input-profile">
+          <div class="form-group" style="width: 304.5px">
+            <label for="Agama-profile">Agama</label>
+            <input type="text" {{ $status ? '' : "disabled" }} wire:model='agama'
+            id="Agama-profile" class="form-control w-100" placeholder="agama">
+          </div>
+
+          <div class="form-group" style="width: 304.5px">
+            <label for="JK-profile">Jenis Kelamin</label>
+            <input type="text" {{ $status ? '' : "disabled" }} wire:model='jk'
+            id="JK-profile" class="form-control w-100" placeholder="jenis kelamin">
+          </div>
+        </div>
+
+        <div class="form-group w-100">
+          <label for="alamat">Alamat</label>
+          <textarea {{ $status ? '' : "disabled" }} wire:model='alamat'
+          id="alamat" rows="3" class="w-100 border p-2 rounded-2" placeholder="alamat anda"></textarea>
+        </div>
+
+      </div>
     </div>
-  </form>
+    
+  </div>
+
+  <div class="col w-100 d-flex justify-content-center" style="height: 36px;">
+      <button class="btn text-neutral-10 text-m-regular w-100"
+      style="background: #4ca1af;" type="submit">Profile Edit</button>
+  </div>
+</form>
